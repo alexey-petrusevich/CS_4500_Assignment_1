ed4c79d6144a8cf5cec13d209eb7659c5d615cac
Aliaksei <apetruesvich1@gmail.com>
2020-01-20 13:08:32 -0500

    Added prototype

---
+++ part1/sorer.h
@@ -0,0 +1,12 @@
+//lang::CwC
+
+// prototypes
+
+// reads a file, a pointer to number of rows, a pointer to numRows and numCols
+// the latter two are used to calculate the number of row and columns
+// while reading a file
+// returns a pointer to the array of c-string pointers
+// containing rows from the file
+char** readFile(const char* filename, int* numRows, int* numCols);
+
+
